696a92308d33e7bee774557c3ab3a1e4681df257
Vidur Khanal <vkhanal@caldwell.edu>
2021-02-10 14:33:45 -0500

    BASIC NEXT JS INIT

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Braket Jobs</title>
+        <link
+          rel="shortcut icon"
+          href="/images/favicon-32x32.png"
+          type="image/x-icon"
+        />
+      </Head>
+    </div>
+  );
+}
