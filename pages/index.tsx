062980d8396e5799a9ba4a9898ae44748e04429f
Vidur Khanal <vkhanal@caldwell.edu>
2021-02-10 15:09:44 -0500

    SOME GIBBERISH REGARDING HOSTNAME/ORIGIN

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,28 @@
+import { FC } from "react";
 import Head from "next/head";
+import { sampleData } from "../utils/types";
 
-export default function Home() {
+interface homeProps {
+  jobs: sampleData;
+  req?: {
+    headers: {
+      host: string;
+    };
+  };
+}
+
+const Home: FC<homeProps> = ({ jobs }) => {
+  let hostname: "localhost" | string =
+    typeof window !== "undefined" && window.location.hostname
+      ? window.location.hostname
+      : "";
+
+  if (hostname === "localhost") {
+    hostname = hostname + ":3000";
+  }
+
+  console.log(hostname);
+
   return (
     <div>
       <Head>
@@ -13,4 +35,21 @@ export default function Home() {
       </Head>
     </div>
   );
+};
+
+export async function getServerSideProps() {
+  const res = await fetch(`http://localhost:3000/api/listingdata`);
+  const jobs: Array<sampleData> = await res.json();
+
+  if (!jobs) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { jobs }, // will be passed to the page component as props
+  };
 }
+
+export default Home;
---
+++ utils/types.ts
@@ -0,0 +1,15 @@
+export type sampleData = {
+  id: number;
+  company: string;
+  logo: string;
+  new: boolean;
+  featured: boolean;
+  position: string;
+  role: string;
+  level: string;
+  postedAt: string;
+  contract: string;
+  location: string;
+  languages: Array<string>;
+  tools: Array<string>;
+};
