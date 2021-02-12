f58d4b1024020ada9baced81fe4d61c6f4280c63
Vidur Khanal <vkhanal@caldwell.edu>
2021-02-11 22:01:09 -0500

    API ROUTING CHANGED AND DEV PATH CREATED

--- pages/api/listingdata.ts
+++ pages/api/listingdata.ts
@@ -1,8 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-const listingData = require("../../data.json");
-
 export default (req: any, res: any) => {
   res.statusCode = 200;
-  res.json(JSON.stringify(listingData));
+  res.json(JSON.stringify("sfsf"));
 };
