c01d7c50cad994a2860792504e462cd1535c6054
Vidur Khanal <vkhanal@caldwell.edu>
2021-02-11 22:00:01 -0500

    NAVBAR CREATED

---
+++ components/NavBar.tsx
@@ -0,0 +1,45 @@
+import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  return (
+    <Flex
+      background="white"
+      h="75px"
+      minH="max-content"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Flex>
+        <a href="/">
+          <Image
+            src="/images/logo.png"
+            alt="Braket Jobs"
+            width="90px"
+            px="1"
+            draggable={false}
+          />
+        </a>
+        <Box paddingY="2">
+          <Text
+            color="green.500"
+            border="1px solid green"
+            borderRadius="20px"
+            paddingX="2"
+            userSelect="none"
+          >
+            Beta
+          </Text>
+        </Box>
+      </Flex>
+      <Box px="10px">
+        <Button colorScheme="teal">
+          <Text as="a" href="/post/new/">
+            Post A Job
+          </Text>
+        </Button>
+      </Box>
+    </Flex>
+  );
+};
