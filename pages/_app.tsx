1c69a89335c5422a7b97274774fc42274fc17151
Vidur Khanal <vkhanal@caldwell.edu>
2021-02-11 22:13:30 -0500

    meta tags added to head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,13 @@ function MyApp({ Component, pageProps }: any) {
       <CSSReset />
       <Head>
         <title>Braket Jobs</title>
+        <meta name="viewport" content="width=device-width ,initial-scale=1" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="robots" content="noindex" />
+        <meta
+          name="description"
+          content="Braket Jobs is a moden and elegant job hunting site made by developers for developers. Currently, it is in developmental stage and new features are being added on a regular basis. The initial intentions for the site was to create a trivago like site for Job hunting. Currentl braket Jobs is not acccepting job posting."
+        />
         <link
           rel="shortcut icon"
           href="/images/favicon-32x32.png"
