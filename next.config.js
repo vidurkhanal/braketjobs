1af9e4f21a2d33901732b30d3ca5229da70b031d
Vidur Khanal <vkhanal@caldwell.edu>
2021-02-15 09:25:21 -0500

    NEXT CONFIG FILE ADDED

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    HomeFetchUrl: "https://jobs.github.com/positions.json?description=code",
+    JobFetchUrl: "https://jobs.github.com/positions/",
+  },
+};
+
+// export {};
