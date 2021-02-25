15f258d6e87986850f1d94857a0691bd84bc26d5
Vidur Khanal <vkhanal@caldwell.edu>
2021-02-25 08:48:13 -0500

    NEXT.CONFIG REMOVED AND CHANGED TO ENV VARIABLES

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  env: {
-    HomeFetchUrl: "https://jobs.github.com/positions.json?description=code",
-    JobFetchUrl: "https://jobs.github.com/positions/",
-    JobSearchUrl: "https://jobs.github.com/positions.json?search=",
-  },
-};
-
-// export {};
